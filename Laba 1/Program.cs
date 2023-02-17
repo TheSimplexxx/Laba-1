94a1220fa64d684bfdc0938596a46107d34cf679
TheSimplexxx <14102003max@gmail.com>
2023-02-17 18:47:12 +0200

    all

--- Laba 1/Program.cs
+++ Laba 1/Program.cs
@@ -12,33 +12,33 @@ namespace Laba1
     {
         static void main()
         {
-            console.writeline("введiть одне з +,-,*,/");
-            string a = console.readline();
-            console.writeline("перше число");
-            int x = convert.toint32(console.readline());
-            console.writeline("друге число");
-            int y = convert.toint32(console.readline());
+            Console.WriteLine("введiть одне з +,-,*,/");
+            string a = Console.ReadLine();
+            Console.WriteLine("перше число");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("друге число");
+            int y = Convert.ToInt32(Console.ReadLine());
             switch (a)
             {
                 case "+":
                     mydelegat mydelegat = (c, b) => c + b;
                     int del = mydelegat(x, y);
-                    console.writeline("вiдповiть:{0}", del);
+                    Console.WriteLine("вiдповiть:{0}", del);
                     break;
                 case "-":
                     mydelegat mydelegat1 = (c, b) => c - b;
                     int del1 = mydelegat1(x, y);
-                    console.writeline("вiдповiть:{0}", del1);
+                    Console.WriteLine("вiдповiть:{0}", del1);
                     break;
                 case "*":
                     mydelegat mydelegat2 = (c, b) => c * b;
                     int del2 = mydelegat2(x, y);
-                    console.writeline("вiдповiть:{0}", del2);
+                    Console.WriteLine("вiдповiть:{0}", del2);
                     break;
                 case "/":
                     mydelegat mydelegat3 = (c, b) => b == 0 ? 0 : c / b;
                     int del3 = mydelegat3(x, y);
-                    console.writeline("вiдповiть:{0}", del3);
+                    Console.WriteLine("вiдповiть:{0}", del3);
                     break;
             }
         }
@@ -53,22 +53,22 @@ namespace Laba1
 
         static int getrandom()
         {
-            random random = new random();
-            int number = random.next(1, 10);
-            console.writeline(number);
+            Random Random = new Random();
+            int number = Random.Next(1, 10);
+            Console.WriteLine(number);
             return number;
         }
 
 
-        static void main()
+        static void Main()
         {
-            console.writeline("введить число елементiв масива: ");
-            int n = int.parse(console.readline());
-            console.writeline(new string('-', 50));
+            Console.WriteLine("введить число елементiв масива: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('-', 50));
 
             var array = new mydelegate[n];
 
-            for (int i = 0; i < array.length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
                 array[i] = () => new mydelegate(getrandom)();
             }
@@ -76,19 +76,19 @@ namespace Laba1
             mydel d = delegate (mydelegate[] a)
             {
                 double sr = 0;
-                for (int i = 0; i < a.length; i++)
+                for (int i = 0; i < a.Length; i++)
                 {
                     sr += a[i]();
                 }
-                return sr / array.length;
+                return sr / array.Length;
             };
 
-            console.writeline("середнє арифметичне = {0}", d(array));
-            console.writeline(new string('-', 50));
+            Console.WriteLine("середнє арифметичне = {0}", d(array));
+            Console.WriteLine(new string('-', 50));
 
-            for (int i = 0; i < array.length; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                console.writeline(array[i]());
+                Console.WriteLine(array[i]());
             }
         }
     }
@@ -117,7 +117,7 @@ namespace Laba1
 
     }
 
-    }
+}
 
 
 
